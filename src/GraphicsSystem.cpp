9441b3e0adb365c79413012f6a3a82ef05a2d2f0
Alun Evans <alun@alunthomasevans.co.uk>
2018-10-16 15:44:00 +0200

    Added comments

--- src/GraphicsSystem.cpp
+++ src/GraphicsSystem.cpp
@@ -17,6 +17,7 @@ void GraphicsSystem::init() {
 	// - initialise state of openGL
 	// - create camera variable in .h and initialise camera here
 
+
 }
 
 void GraphicsSystem::update(float dt) {
@@ -40,8 +41,6 @@ void GraphicsSystem::renderMeshComponent_(Mesh& comp) {
 
 }
 
-
-
 //********************************************
 // OpenGL helper functions
 //********************************************
@@ -60,6 +59,7 @@ int GraphicsSystem::getShaderProgram(std::string name) {
 //creates a standard plane geometry
 void GraphicsSystem::createPlaneGeometry(GLuint& vao, GLuint& num_tris) {
 
+	//four vertices in a square
 	const GLfloat position_buffer_data[] = {
 		-0.5f, -0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
@@ -67,43 +67,50 @@ void GraphicsSystem::createPlaneGeometry(GLuint& vao, GLuint& num_tris) {
 		-0.5f, 0.5f, 0.0f
 	};
 
+	//texture coordinates (uvs)
 	const GLfloat texture_buffer_data[] = {
 		0.0f, 0.0f,
-		0.0f, 1.0f,
+		1.0f, 0.0f,
 		1.0f, 1.0f,
-		1.0f, 0.0f
+		0.0f, 1.0f
 	};
-
+	//normals point 'out of screen'
 	const GLfloat normal_buffer_data[] = {
 		0.0f, 0.0f, 1.0f,
 		0.0f, 0.0f, 1.0f,
 		0.0f, 0.0f, 1.0f,
 		0.0f, 0.0f, 1.0f
 	};
+	//index buffer
+	const GLuint index_buffer_data[] = { 0, 1, 2, 0, 2, 3 };
 
-	const GLuint index_buffer_data[] = { 0, 1, 2,0, 2, 3 };
+	//set number of triangles (of passed variable)
+	num_tris = 2;
 
+	//create Vertex Array Object
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
+	//Now create Vertex Buffer Objects for each buffer: positions, uvs, normals, and indices
 	GLuint vbo;
 	//positions
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(position_buffer_data), position_buffer_data, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glGenBuffers(1, &vbo); //create buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vbo); //set OpenGL state to say we want to work with this buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(position_buffer_data), position_buffer_data, GL_STATIC_DRAW); //copy data
+	glEnableVertexAttribArray(0); //enable attribute labelled as '0' in vertex shader: "layout(location = 0) in vec3 a_vertex;"
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); //each attribute '0' is composed of 3 floats (in this case, xyz)
+
 	//texture coords
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(texture_buffer_data), texture_buffer_data, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(1); // (1 = a_uv)
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0); //only two coordinates for textures: uv
 	//normals								   
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(normal_buffer_data), normal_buffer_data, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(2);
+	glEnableVertexAttribArray(2); // (2 = a_normal)
 	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	//indices
 	GLuint ibo;
@@ -114,5 +121,5 @@ void GraphicsSystem::createPlaneGeometry(GLuint& vao, GLuint& num_tris) {
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
-	num_tris = 2;
+
 }
